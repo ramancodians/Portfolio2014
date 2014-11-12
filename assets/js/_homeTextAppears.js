01b23be6d6be9de7b7d591ec5fa73626ee675329
AlexisBertin <mail@alexisbertin.fr>
2014-11-12 04:16:44 +0100

    v3.9 - Fucked
    Tired.

--- assets/js/_homeTextAppears.js
+++ assets/js/_homeTextAppears.js
@@ -1,76 +1,19 @@
-$(document).ready(function(){
-
-	function navigateToPage(pageName, title) {
-	    /*var pageName = getPageName();*/
-	    $.get('index.html', function (response) {
-	        var markup = $("<div>" + response + "</div>"),
-	            fragment = markup.find("section").html();
-	        $("section").html(fragment).addClass(title+" opacity");
-	    });
-	}
-
-	$("a[data-role='ajax']").click(function (e) {
-		e.preventDefault();
-	    if (Modernizr.history) {
-	        $('.home .homeMenu li a').not(this).addClass('homeMenu_jsActive');
-	        var thisPage =  $(this);
-	        var thisSection = $(this).parents('section');
-	        thisSection.removeClass('opacity');
-
-	        window.setTimeout(function(){
-	        	thisSection.removeClass();
-	         	var pageName = thisPage.attr("href");
-	         	var title = pageName.slice(0, -1);
-	         	window.history.pushState(null, "", pageName);
-	         	navigateToPage(pageName, title);
-	        }, 300);
-	    } else { console.log('no Modernizr History'); }
-	});
-
-
-	/*var _popStateEventCount = 0;*/
-	$(window).on('popstate', function (e) {
-	    this._popStateEventCount++;
-	    /*if ($.browser.webkit && this._popStateEventCount == 1) {
-	        return;
-	    }*/
-	    var oldPageName = window.location.pathname;
-	    oldPageName = oldPageName.split("/");
-	    oldPageName = oldPageName.filter(function(n){ return n != "" });
-
-	    $('section').removeClass('opacity');
-	    window.setTimeout(function(){
-	    	$('section').removeClass();
-	    	
-	    	if(oldPageName.length > 1){
-	    		var title = oldPageName[oldPageName.length - 1];
-	    		var pageName = title+'/';
-	    	} else {
-	    		var pageName = '../';
-	    		var title = 'home';
-	    	}
-	    	navigateToPage(pageName, title);
-	    }, 300);
-	});
-
-
-
-
-
-/* ---------------- Home ---------------- */
-	
-	$('.home-btStartUx').hide().css({
+$('.home-btStartUx').hide().css({
 		'-webkit-transform': 'translate(-50%,10px)',
 		'-ms-transform': 'translate(-50%,10px)',
 		'transform': 'translate(-50%,10px)'
 	});
 	var homeTitle = $('.home .homeTitle').html();
+
+	// Get the content before clear the div
 	var homeTitleContent = [
 		$('.home .homeTitle p:first-of-type').text().split(""),
-		$('.home .homeTitle h1').text().split(""),
+		$('.home .homeTitle h1').html(),
 		$('.home .homeTitle p:last-of-type').text().split(""),
 		$('.home .homeTitle h2').text().split("")
 	]
+
+	// Ending function, reset the div
 	function killHomeFunction(){
 		w = 0;
 		$('home .homeTitle h2 span').css({'color':'rgba(255,255,255,.8)'});
@@ -85,6 +28,8 @@ $(document).ready(function(){
 		}).dequeue();
 		return w;
 	}
+
+
 	$('.home .homeTitle')
 		.empty()
 		.delay(1000)
@@ -108,7 +53,7 @@ $(document).ready(function(){
 					}
 					
 					setTimeout(function(){
-							if(homeTitleContent[w][x] == " "){
+							if((homeTitleContent[w][x] == " ") && (w != 1)){
 								x++;
 								$('.home .homeTitle '+homeTitleAttr).append(' <span>'+homeTitleContent[w][x]+'</span>');
 							} else {
@@ -118,9 +63,12 @@ $(document).ready(function(){
 								'opacity':'0',
 								'color': 'rgba(255,255,255,1)'
 							});
-							$
-							if(w == 1){
-								$('.home .homeTitle '+homeTitleAttr+' span:last-of-type')
+							if(w == 1 && x == 0){
+							/*	$.get("assets/svg/svgdefs.svg #homeAlexis", function(data){
+									$('.home .homeTitle h1').append('<svg>'+data+'</svg>');
+								});*/
+								
+								/*$('.home .homeTitle '+homeTitleAttr+' span:last-of-type')
 									.css({
 										'color': '#f4fd14'
 									})
@@ -129,7 +77,8 @@ $(document).ready(function(){
 										$(this).css({
 											'opacity':'1'
 										});		
-									});					
+									});	*/
+								w++; x = 0;		
 							} else if(w == 3){
 								$('.home .homeTitle '+homeTitleAttr+' span:last-of-type')
 									.css({'opacity':'1','font-weight':'500'})
@@ -185,43 +134,3 @@ $(document).ready(function(){
 			}
 			lightMyWord(w, x);
 		});
-
-/* -------------------------------------- */
-
-
-
-
-/* ---------------- Others ---------------- */
-	/* Calcul de la taille du menu en fonction du nombre de li et de leur taille individuelle */
-
-	/*var navMenuWidth = 0;
-	for(var x = 1; x <= $('.works .mainNav li').size(); x++){
-		navMenuWidth = navMenuWidth + $('.works .mainNav li:nth-child('+x+') a').width() + 26;
-	}
-
-	var homeMenuWidth = 0;
-	for(var x = 1; x <= $('.home .homeMenu li').size(); x++){
-		homeMenuWidth = homeMenuWidth + $('.home .homeMenu li:nth-child('+x+') a').width() + 26;
-	}
-	console.log(homeMenuWidth);
-	$('.home .homeMenu').css('width',homeMenuWidth);
-	$('.works .mainNav').css('width',navMenuWidth);*/
-/* ---------------------------------------- */
-
-
-
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
