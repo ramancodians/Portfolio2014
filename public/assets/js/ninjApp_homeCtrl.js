270c1e28ac9c9603a8f3e0aa31135cdd5558aa6d
AlexisBertin <mail@alexisbertin.fr>
2015-02-23 17:07:28 +0100

    v0.5.4

--- public/assets/js/ninjApp_homeCtrl.js
+++ public/assets/js/ninjApp_homeCtrl.js
@@ -5,7 +5,7 @@ ninjApp.controller('homeController', function($scope, Page){
 
 	Page.setTitle('Alexis Bertin | Front-End Developer');
 
-	TweenLite.to('.overlay', 0.2, {background: "rgba(0,0,0,0.8)"});
+	TweenLite.to('.overlay', 0.2, {background: "rgba(0,0,0,0.6)"});
 	TweenLite.to('.background', 0.2, {opacity: "1"});
 
 	if (document.documentElement.clientWidth > 1100) {
