b84798450b641f96c221b4521e5b9af7578ad43c
AlexisBertin <mail@alexisbertin.fr>
2015-04-24 15:33:24 +0100

    v1.2

--- server.js
+++ server.js
@@ -2,18 +2,14 @@
 	var express = require('express');
 	var app = express();
 	var compression = require('compression');
-
 	var oneDay = 86400000;
-
-
-	var mongoose = require('mongoose');
-	var db = require('./config/db');
+	/*var mongoose = require('mongoose');
+	var db = require('./config/db');*/
 	var port = process.env.PORT || 8080;
 	var path = require('path');
 	var morgan = require('morgan');
 	var bodyParser = require('body-parser');
 	var methodOverride = require('method-override');
-
 	var nodemailer = require('nodemailer');
 // ======================================================================================
 
@@ -22,18 +18,10 @@
 
 
 // Configuration ========================================================================
-	mongoose.connect(db.url);
-
+	/*mongoose.connect(db.url);*/
 	/*app.use(compression());*/
 	/*app.use(express.static(__dirname + '/public'));*/
-
-
 	app.use(express.static(path.join(__dirname, '/public')));
-	
-
-	
-
-
 	app.use(morgan('dev')); // Log every request to the console
 	app.use(bodyParser.urlencoded({'extended':'true'}));
 	app.use(bodyParser.json());
@@ -43,9 +31,6 @@
 
 
 
-
-
-	
 	var transporter = nodemailer.createTransport({
 	    service: 'Gmail',
 	    auth: {
@@ -53,7 +38,6 @@
 	    	pass: ""
 		}
 	});
-	 
 	app.post('/sendMessage', function(req, res) {
 		var mailOptions = {
 		    from: req.body.mail,
@@ -62,7 +46,6 @@
 		    html: '<p style="font-size: 16px; line-height: 24px;"><span style="font-weight:bold">Name: </span> <span style="color: #15c">'+req.body.name+'</span> <br /><span style="font-weight:bold">Mail: </span> <span style="color: #15c">'+req.body.mail+'</span> <br /><span style="font-weight:bold">Message: </span> <br /><span style="color: #15c">'+req.body.message+'</span></p>',
 		    text: req.body.name + ' | ' + req.body.mail + ' | ' + req.body.message
 		};
-		
 		transporter.sendMail(mailOptions, function (error, info) {
 		    if (error) {
 		        res.send('NOPENOPEN: '+error);
@@ -71,8 +54,6 @@
 		    }
 		});
 	});
-
-
 	app.get('*', function(req, res){
 		res.sendfile('./public/index.html');
 	});	
@@ -83,14 +64,4 @@
 	app.listen(port);
 	console.log('App listening on port **'+port+'**');
 // ======================================================================================
-
-
-
-
-
-
-
-
-
-
 
